c4cc0e3926c6f7206c9d79f6a4cab17aec971749
Jose <64102720+Jose-Rodolfo-Llave@users.noreply.github.com>
2022-07-13 18:52:41 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -21,15 +21,29 @@ public class Exercise1 {
 
 
     public <T extends Comparable<T>> boolean bstSimilares(BST<T> a1, BST<T> a2){
-        //Si ambos nodos son nulos quiere decir que el nodo padre no tiene hijos es decir, su
+       //Si ambos estan vacios retorna verdadero.
+    	if(a1.isEmpty() && a2.isEmpty()) {
+    		return true;
+    	//Si uno de los arboles esta vacio y el otro no retornara falso
+    	} else if (a1.isEmpty() || a2.isEmpty()){
+    		return false;
+    	//Si ambos no es tan vacios se comprueba la similitud
+    	} else {
+    		return bstSimilares(a1.root, a2.root);
+    	}
+    }
+    
+    public <T extends Comparable<T>> boolean bstSimilares(Node<T> a1, Node<T> a2){
+    	//Si ambos nodos son nulos quiere decir que el nodo padre no tiene hijos es decir, su
     	//nodo antecesor es un nodo hoja.
     	if(a1 == null && a2 == null) {
     		return true;
-        //Basta con que un nodo de cualquier arbol sea nulo y el otro, y viceversa, retornara false
+    	//Basta con que un nodo de cualquier arbol sea nulo y el otro, y viceversa, retornara false
     	} else if((a1!= null && a2 == null) || (a1 == null && a2 != null)) {
     		return false;
-    	    }
-    	    //Si son nodos padre pasaran a los hijos mediante recursividad
+    	}
+    	//Si son nodos padre pasaran a los hijos mediante recursividad
     	return bstSimilares(a1.left, a2.left) && bstSimilares(a1.right, a2.right);
+
     }
 }
